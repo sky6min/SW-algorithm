ce7cbaa06e77ea64cfcff09c4191f7791e4cee0b
sky6min <sky6min@gmail.com>
2021-10-22 23:48:27 +0900

    백준 문제 2개 업로드

---
+++ src/test1/BJ_나무자르기_2805.java
@@ -0,0 +1,8 @@
+package test1;
+
+public class BJ_나무자르기_2805 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test1/bj_물통_2251.java
@@ -0,0 +1,12 @@
+package test1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj_물통_2251 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
