bad5e86956c46544071b8463d45acf784eb78c55
sky6min <sky6min@gmail.com>
2021-10-25 22:47:31 +0900

    백준 문제 1개 로직 수정

--- src/test1/BJ_나무자르기_2805.java
+++ src/test1/BJ_나무자르기_2805.java
@@ -1,8 +1,55 @@
 package test1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BJ_나무자르기_2805 {
 
-    public static void main(String[] args) {
+    static int max = Integer.MIN_VALUE;
+    static int ary[];
+    static int result = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ary = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            ary[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(ary[i], max);
+        }
+
+        int top = max;
+        int bottom = 0;
+
+        while(bottom<top) {
+            int cut = (top + bottom) / 2;
+            long res = 0;
+
+            for(int i=0; i<n; i++) {
+                if(ary[i] - cut >= 0) {
+                    res += (ary[i] - cut);
+
+                }
+
+            }
+
+            if(res >= m) {
+                bottom = cut+1;
+
+            }
+            if(res < m) {
+                top = cut;
+
+            }
+
+        }
+
+        System.out.println(bottom - 1);
 
     }
 }
--- src/test1/bj_물통_2251.java
+++ src/test1/bj_물통_2251.java
@@ -2,6 +2,7 @@ package test1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -12,7 +13,7 @@ public class bj_물통_2251 {
     static boolean visited[][][] = new boolean[3][3][201];
 
     static int src[] = new int[3];
-
+    static ArrayList<Integer> al = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,8 +26,9 @@ public class bj_물통_2251 {
         int mount[] = new int[3];
         mount[0] = 0;
         mount[1] = 0;
-        mount[2] = 10;
+        mount[2] = src[2];
         Node node = new Node(2, mount);
+        al.add(src[2]);
         queue.offer(node);
 
 
@@ -35,18 +37,23 @@ public class bj_물통_2251 {
         while(!queue.isEmpty()) {
             Node n = queue.poll();
             // cm -> am  cm의 물 양이 더 많은 경우
-            for(int i=0; i<2; i++) {
-                if(node.i == i) continue;
-//                if()
-
+            for(int i=0; i<3; i++) {
+                if(n.i == i) continue;
+                if(visited[n.i][i][src[i]-n.mount[i]]) continue;
+                visited[n.i][i][src[i]-n.mount[i]] = true;
+                int mt[] = new int[3];
+                mt[n.i] = n.mount[n.i] - (src[i] - n.mount[i]);  // from 여기 공식 잘못됨  클 경우랑 작을 경우 나눠야함
+                mt[i] = src[i];  // to
+                queue.offer(new Node(i, mt));
+                if(mt[0] == 0) {
+                    al.add(mt[2]);
+                }
             }
 
-            // cm의 물 양이 더 적은 경우
 
-            // cm -> bm cm의 물 양이 더 많은 경우
-            // cm의 물 양이 더 적은 경우
-
-
+        }
+        for(int i=0; i<al.size(); i++) {
+            System.out.print(al.get(i) + " ");
         }
     }
 
