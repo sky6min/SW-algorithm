5e908b392f0fa432ef9db8f38cbeb3645b0b86c0
sky6min <sky6min@gmail.com>
2021-11-08 23:31:53 +0900

    N번째큰수 2075

--- src/test1/bj_N번째큰수_2075.java
+++ src/test1/bj_N번째큰수_2075.java
@@ -3,7 +3,7 @@ package test1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-// 다음할거
+// 다음할거 임
 public class bj_N번째큰수_2075 {
 
     static PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
