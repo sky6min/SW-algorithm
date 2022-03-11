635acfcf7636613681d9393bb224b162f354f341
sky6min <sky6min@gmail.com>
2022-03-11 23:13:59 +0900

    PG_비밀지도

--- src/test1/PG_비밀지도.java
+++ src/test1/PG_비밀지도.java
@@ -1,22 +1,9 @@
 package test1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
+public class PG_비밀지도 {
+    char map[][];
 
-public class PG_연습장 {
-
-
-
-    public static void main(String[] args) {
-        int[] orders = {9, 20, 28, 18, 11};
-        int[] courses = {30, 1, 21, 17, 28};
-        solution(5,orders, courses);
-    }
-
-    static char map[][];
-
-    public static String[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         map = new char[n][n];
         for(int i=0; i<n; i++) {
@@ -25,8 +12,6 @@ public class PG_연습장 {
 
             }
         }
-
-
         for(int i=0; i<n; i++) {
             String hint1 = Integer.toBinaryString(arr1[i]);
             String hint2 = Integer.toBinaryString(arr2[i]);
@@ -34,12 +19,10 @@ public class PG_연습장 {
             char[] h2 = hint2.toCharArray(); // 11110
             int hIdx1 = h1.length-1;
             int hIdx2 = h2.length-1;
-            System.out.println(hIdx2);
-            for(int j=n-1; j>=0; j++) {
-                if(hIdx1 == -1) {}
+            for(int j=n-1; j>=0; j--) {
+                if(hIdx1 <= -1) {}
                 else if(h1[hIdx1--] == '1') map[i][j] = '#';
-                if(hIdx2 == -1) {}
-
+                if(hIdx2 <= -1) {}
                 else if(h2[hIdx2--] == '1') map[i][j] = '#';
             }
 
@@ -50,8 +33,4 @@ public class PG_연습장 {
 
         return answer;
     }
-
 }
-
-
-
