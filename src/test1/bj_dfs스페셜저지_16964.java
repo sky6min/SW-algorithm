bc375e1816e1dab528462d5449e27322e9b167e2
sky6min <sky6min@gmail.com>
2022-04-19 23:21:25 +0900

    BJ_dfs스페셜저지_16964 solved

--- src/test1/bj_dfs스페셜저지_16964.java
+++ src/test1/bj_dfs스페셜저지_16964.java
@@ -1,8 +1,20 @@
 package test1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class bj_dfs스페셜저지_16964 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            if(st.hasMoreTokens()) {
+
+            }
+        }
 
     }
 }
