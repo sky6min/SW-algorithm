5d4216497037166e90cc57963931a69da70db37b
sky6min <sky6min@gmail.com>
2022-05-03 00:57:17 +0900

    BJ_bfs스페셜저지_16940 solved

---
+++ src/test1/hashsetTest.java
@@ -0,0 +1,11 @@
+package test1;
+
+import java.util.HashSet;
+
+public class hashsetTest {
+
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+    }
+}
